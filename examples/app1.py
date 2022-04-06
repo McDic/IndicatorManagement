da05c8043489074fc2570728c970177906971e33
McDic <spongbob9876@naver.com>
2022-04-06 22:14:21 +0900

    Added the first example

---
+++ examples/app1.py
@@ -0,0 +1,30 @@
+import random
+
+import matplotlib.pyplot as plt
+
+from indicator_management.indicators import RawSeriesIndicator
+from indicator_management.orchestrate import generate
+
+
+def main():
+
+    i1 = RawSeriesIndicator(raw_values=(random.randint(1, 10) for _ in range(10)))
+    i2 = i1 * 2 + 1
+
+    y_total = list(generate(y1=i1, y2=i2))
+    y1 = [obj["y1"] for obj in y_total]
+    y2 = [obj["y2"] for obj in y_total]
+
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(y1, "r", label="y1")
+    ax.plot(y2, "g", label="y2")
+    ax.legend()
+
+    w, h = fig.get_size_inches()
+    fig.set_size_inches(w * 4, h * 2)
+    fig.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
