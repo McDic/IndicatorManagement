f940cc972f013b240157d1572ea25abae2be5a63
McDic <spongbob9876@naver.com>
2022-04-09 10:51:44 +0900

    Implemented variance and Bollinger Band

--- src/indicator_management/indicators/financial.py
+++ src/indicator_management/indicators/financial.py
@@ -1,4 +1,8 @@
-from .base import AbstractIndicator, AbstractHistoryTrackingIndicator, Numeric
+from .base import (
+    AbstractIndicator,
+    AbstractHistoryTrackingIndicator,
+    Numeric,
+)
 
 
 class SimpleMovingAverage(AbstractHistoryTrackingIndicator[Numeric]):
@@ -23,3 +27,45 @@ class SimpleMovingAverage(AbstractHistoryTrackingIndicator[Numeric]):
             if self._tracking_length > self._none_count
             else None
         )
+
+
+class SimpleMovingVariance(AbstractHistoryTrackingIndicator[Numeric]):
+    """
+    Variance indicator. Exact formula of this indicator is
+    `Var(source, len) = variance(filter(None, source), len - none_count)`.
+    If all values in tracking history are `None`, then produces `None`.
+    """
+
+    def __init__(self, indicator: AbstractIndicator, length: int) -> None:
+        super().__init__(indicator, length, default_value=None)
+        self._sum = 0
+        self._square_sum = 0
+
+    def update_single_before_history_shift(self) -> None:
+        self._sum -= self._removed_value or 0.0  # type: ignore
+        self._square_sum -= (self._removed_value or 0.0) ** 2  # type: ignore
+
+    def update_single_after_history_shift(self) -> None:
+        value = self.pre_requisites[0](0) or 0.0
+        self._sum += value  # type: ignore
+        self._square_sum += value**2  # type: ignore
+        length = self._tracking_length - self._none_count
+        self.value = (
+            self._square_sum / length - (self._sum / length) ** 2
+            if length > 0
+            else None
+        )
+
+
+def bollinger_band(
+    base_indicator: AbstractIndicator[Numeric],
+    length: int,
+    stdev_multiplier: float = 2.0,
+) -> tuple[AbstractIndicator[Numeric], ...]:
+    """
+    Return upper bound, middle, and lower bound of Bollinger Band.
+    """
+    assert stdev_multiplier > 0
+    moving_average = SimpleMovingAverage(base_indicator, length)
+    bandwidth = SimpleMovingVariance(base_indicator, length) ** 0.5 * stdev_multiplier
+    return moving_average + bandwidth, moving_average, moving_average - bandwidth
