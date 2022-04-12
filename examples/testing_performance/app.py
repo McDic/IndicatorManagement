2137473e79427f44a6f702156b4ab3b82d8a753e
McDic <spongbob9876@naver.com>
2022-04-13 02:39:17 +0900

    Add comparison for `safe_none` between True and False

--- examples/testing_performance/app.py
+++ examples/testing_performance/app.py
@@ -3,16 +3,26 @@ import os
 import pstats
 from random import random
 
-from indicator_management import RawSeriesIndicator, generate_sync
+from indicator_management import (
+    RawSeriesIndicator,
+    generate_sync,
+    multiplication,
+    summation,
+)
 
 
-def main(count: int = 10**6):
+def main(count: int = 10**5, safe_none: bool = True):
     def generate_forever():
         while True:
             yield random()
 
     raw_series = RawSeriesIndicator(raw_values=generate_forever())
-    double_x_plus_1 = raw_series * 2 + 1
+    double_x_plus_1 = summation(
+        multiplication(raw_series, 2, start=1, safe_none=safe_none),
+        1,
+        start=0,
+        safe_none=safe_none,
+    )
 
     generate = generate_sync(i=double_x_plus_1)
     for _ in range(count):
@@ -20,7 +30,11 @@ def main(count: int = 10**6):
 
 
 if __name__ == "__main__":
-    cProfile.run("main()", "temp.perf")
-    p = pstats.Stats("temp.perf")
-    p.strip_dirs().sort_stats("tottime").print_stats(100)
-    os.remove("temp.perf")
+    cProfile.run("main(safe_none=True)", "safe_none_yes.perf")
+    cProfile.run("main(safe_none=False)", "safe_none_no.perf")
+    p_yes = pstats.Stats("safe_none_yes.perf")
+    p_no = pstats.Stats("safe_none_no.perf")
+    p_yes.strip_dirs().sort_stats("tottime").print_stats(20)
+    p_no.strip_dirs().sort_stats("tottime").print_stats(20)
+    os.remove("safe_none_yes.perf")
+    os.remove("safe_none_no.perf")
